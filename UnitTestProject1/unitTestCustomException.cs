c0abd9e12f68f1f3d92795db4d410987c19ffca0
test1MC <test1@madcapsoftware.com>
2020-09-17 13:21:46 -0500

    Initial Commit

---
+++ UnitTestProject1/unitTestCustomException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    class unitTestCustomException : Exception
+    {
+        public unitTestCustomException()
+        {
+
+        }
+        public unitTestCustomException(String message) : base(message)
+        {
+
+        }
+        public unitTestCustomException(String message, Exception inner) : base(message, inner)
+        {
+
+        }
+
+        public unitTestCustomException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+
+        }
+
+    }
+}
---
+++ UnitTestProject1/unitTestWebPageInteraction.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace UnitTestProject1
+{
+    class unitTestWebPageInteraction
+    {
+        public void validatePageTitle(IWebDriver driver, String strExpected)
+        {
+            Assert.AreEqual(driver.Title.ToString(), strExpected); 
+        }
+
+        public void filledInformation(String strValue, String whatElement)
+        {
+            Console.WriteLine("Filled Field: {0} with value: \"{1}\"", whatElement, strValue);
+        }
+
+        public void validateText(String actualStr, String expectedStr)
+        {
+            if (actualStr.Trim() == expectedStr.Trim())
+            {
+                Console.WriteLine($"Custom Assert. Successful Assert! The text \"{actualStr}\" is equal to \"{expectedStr}\"");
+            }
+            else
+            {
+                throw new unitTestCustomException($"Custom Exception Message. Custom Assert. Failed Assert: The text \"{actualStr}\" is different compared with \"{expectedStr}\"");
+            }
+        }
+
+}
+}
